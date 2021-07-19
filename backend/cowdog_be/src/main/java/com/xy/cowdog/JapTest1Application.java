0c9aa6085317d060992851bfdfb437aaa3cb7843
rlawjddns0 <rlawjddns0@naver.com>
2021-07-19 10:29:58 +0900

    init

---
+++ backend/cowdog_be/src/main/java/com/xy/cowdog/JapTest1Application.java
@@ -0,0 +1,13 @@
+package com.xy.cowdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapTest1Application.class, args);
+	}
+
+}
