1704ee9659c3367b87273fb61df059c64611a2f9
Seongho <skk7541@gmail.com>
2021-07-23 16:03:58 +0900

    add element-ui

---
+++ cowdog_fe/src/components/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-button type="text" @click="outerVisible = true">open the outer Dialog</el-button>
+  <el-dialog title="Outer Dialog" v-model="outerVisible">
+    <template #default>
+      <el-dialog
+          width="30%"
+          title="Inner Dialog"
+          v-model="innerVisible"
+          append-to-body>
+      </el-dialog>
+    </template>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="outerVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="innerVisible = true">open the inner Dialog</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        outerVisible: false,
+        innerVisible: false
+      };
+    }
+  }
+</script>
