8b317601a73b1c0017333b6aefe3e17323a7b69f
오웅식 <woongsik.oh@gmail.com>
2021-07-26 10:15:58 +0900

    S05P12B104-102/backend import 후 webRTC branch 생성, push test

--- backend/cowdog_be/src/main/java/com/xy/cowdog/JapTest1Application.java
+++ backend/cowdog_be/src/main/java/com/xy/cowdog/JapTest1Application.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JapTest1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JapTest1Application.class, args);
 	}
 
 }
+
+//webRTC commit test
