b83df0ae5c6331b2d6480bf3dc63751d0a35296e
Seongho <skk7541@gmail.com>
2021-08-02 16:50:45 +0900

    #S05P13B104-63, #S05P13B104-64 게시글 목록 읽기 + 게시글 신고 기능 구현

---
+++ backend/cowdog_be/src/main/java/com/xy/controller/ArticleReportController.java
@@ -0,0 +1,51 @@
+package com.xy.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xy.api.request.ArticleReportPostReq;
+import com.xy.common.response.BaseResponseBody;
+import com.xy.entity.ArticleReport;
+import com.xy.service.ArticleReportService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("/cowdog/article-report")
+@CrossOrigin(origins="http://localhost:8083")
+public class ArticleReportController {
+
+	@Autowired
+	ArticleReportService articleReportService;
+	
+	@PostMapping("") // '/user-report'에 post 요청
+	@ApiOperation(value = "게시글 신고", notes = "게시글을 신고한다.") 
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "성공"),
+        @ApiResponse(code = 401, message = "인증 실패"),
+        @ApiResponse(code = 404, message = "사용자 없음"),
+        @ApiResponse(code = 500, message = "서버 오류")
+    })
+	public ResponseEntity<? extends BaseResponseBody> create(
+			@RequestBody ArticleReportPostReq request) {
+		System.out.println(request);
+		if(articleReportService.createArticleReport(request).equals("FAIL")) {
+			return ResponseEntity.status(200).body(BaseResponseBody.of(404, "FAIL"));
+		}
+		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "SUCCESS"));
+	}
+	
+	@GetMapping("") // article-report에 get 요청
+	public List<ArticleReport> get() {
+		return articleReportService.getArticleReportList();
+	}
+	
+}
--- backend/cowdog_be/src/main/java/com/xy/entity/ArticleReport.java
+++ backend/cowdog_be/src/main/java/com/xy/entity/ArticleReport.java
@@ -2,6 +2,7 @@ package com.xy.entity;
 
 import java.sql.Timestamp;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,14 +15,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserReport {
+public class ArticleReport {
 	
 	@Id @GeneratedValue
-	private Long userReportNo;
+	private Long ArticleReportNo; // 신고 게시물 순서 넘버링
 	
-	private String reportedId;//누가 신고 대상인지
+	private String reportedArticleNo;//어떤 게시글이 신고 대상인지
 	private String title;
 	private String content;
 	private Timestamp regtime;
-	private String image;
+	private String articleUrl;
 }
